0b68428678c098c08fd678a262c797eb6580e190
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-02 20:48:03 +0100

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,8 +4,9 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
-  }
+    queue.push(error.toString());
+  }finally{
   queue.push('Guardrail was processed');
+  }
   return queue;
 }
