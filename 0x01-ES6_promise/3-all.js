76ff4156058327e87313f5046251958cc0682da1
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-02 19:15:02 +0100

    task 4 adjuested

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
       const photo = result[0];
       const user = result[1];
