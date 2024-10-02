862946775829001904a1f995a32922b168d0f6b3
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-02 19:10:28 +0100

    task 4

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((result) => {
+      const photo = result[0];
+      const user = result[1];
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
