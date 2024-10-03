3dc952fdaeab13515264fe68e6ac448a3b49def8
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-03 16:33:09 +0100

    task 3 and 2

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,26 +1,38 @@
 export default class Currency {
-  constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    constructor(code, name) {
+      if (typeof code !== 'string') {
+        throw new TypeError('Code must be a string');
+      } else if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+  
+      this._code = code;
+      this._name = name;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    get code() {
+      return this._code;
+    }
+  
+    set name(name) {
+      if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = name;
+    }
+  
+    set code(code) {
+      if (typeof code !== 'string') {
+        throw new TypeError('Code must be a string');
+      }
+      this._code = code;
+    }
+  
+    displayFullCurrency() {
+      return `${this._name} (${this._code})`;
+    }
   }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(value) {
-    this._code = value;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    this._name = value;
-  }
-
-  displayFullCurrency() {
-    return `${this._name} (${this._code}) `;
-  }
-}
