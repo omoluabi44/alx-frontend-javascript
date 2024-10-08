df94cdd0987a656582e368e36098e91475e56893
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-08 10:21:41 +0100

    task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
   if (position < 0 || position >= int8View.length) {
-    throw new Error('position Outside range');
+    throw new Error('Position outside range');
   }
 
   int8View[position] = value;
